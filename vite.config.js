041b7b53542cf51b3d58ba504b894a78ed607f03
snomiao <snomiao@gmail.com>
2024-04-03 11:57:58 +0000

    fix(path): relative
    relative

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,6 @@ import tailwindcss from "tailwindcss";
 export default defineConfig({
   plugins: [tailwindcss()],
   manifest: true, // uncomment for PWA support
+  base: "./",
+  build: { outDir: "./docs" },
 });
